d60cc24d3bd53e93af7283f469a096fb7a8c7473
yuqing <yuqing@yongche.com>
2017-03-03 17:36:43 +0800

    call php serialize func with TSRMLS_CC

--- php-shmcache/serializer/php.c
+++ php-shmcache/serializer/php.c
@@ -13,7 +13,7 @@ int shmcache_php_pack(zval *pzval, smart_str *buf)
 	php_serialize_data_t var_hash;
 
 	PHP_VAR_SERIALIZE_INIT(var_hash);
-	php_var_serialize(buf, GET_ZVAL_PTR(pzval), &var_hash);
+	php_var_serialize(buf, GET_ZVAL_PTR(pzval), &var_hash TSRMLS_CC);
 	PHP_VAR_SERIALIZE_DESTROY(var_hash);
     return 0;
 }
@@ -26,7 +26,7 @@ int shmcache_php_unpack(char *content, size_t len, zval *rv)
 	p = (const unsigned char*)content;
 	ZVAL_FALSE(rv);
 	PHP_VAR_UNSERIALIZE_INIT(var_hash);
-	if (!php_var_unserialize(GET_ZVAL_PTR(rv), &p, p + len,  &var_hash)) {
+	if (!php_var_unserialize(GET_ZVAL_PTR(rv), &p, p + len,  &var_hash TSRMLS_CC)) {
 		zval_ptr_dtor(GET_ZVAL_PTR(rv));
 		PHP_VAR_UNSERIALIZE_DESTROY(var_hash);
 		logError("file: "__FILE__", line: %d, "
